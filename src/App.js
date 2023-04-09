6669c6fb8afadd45e55d59e4a4bd3e90fb0c7277
mttaskin <0mttaskin1@gmail.com>
2023-04-09 11:07:24 +0300

    last

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Nav from "./components/Nav"
 import Contact from "./pages/Contact"
 import Home from "./pages/Home"
 import People from "./pages/People"
+import PersonDetail from "./pages/PersonDetail"
 import NotFound from "./pages/NotFound"
 import { Routes, Route } from "react-router-dom"
 import Paths from "./pages/Paths"
-import PersonDetail from "./pages/PersonDetail"
+import FullStack from "./pages/Fullstack"
+import Aws from "./pages/Aws"
+import ReactJS from "./pages/ReactJS"
+import Login from "./pages/Login"
+import PrivateRouter from "./pages/PrivateRouter"
 
 function App() {
   return (
@@ -16,8 +21,23 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People />} />
         <Route path="/people/:id" element={<PersonDetail />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/paths" element={<Paths />} />
+
+        <Route path="/paths" element={<Paths />}>
+          <Route path="fullstack" element={<FullStack />}>
+            <Route path="react" element={<ReactJS />} />
+          </Route>
+          <Route path="" element={<Aws />} />
+        </Route>
+
+        <Route path="/contact" element={<PrivateRouter />}>
+          <Route path="" element={<Contact />} />
+        </Route>
+
+        {/* <Route path="/blog" element={<PrivateRouter />}>
+          <Route path="" element={<Blog />} />
+        </Route> */}
+
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
 
